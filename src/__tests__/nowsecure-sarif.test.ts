8e48d4465093fc924d793d196445266d94562d2b
Keegan Saunders <ksaunders@nowsecure.com>
2022-08-19 13:38:48 -0400

    Use HTTP GET with retries for Platform

--- src/__tests__/nowsecure-sarif.test.ts
+++ src/__tests__/nowsecure-sarif.test.ts
@@ -10,6 +10,7 @@ import { convertToSarif } from "../nowsecure-sarif";
 import { NowSecure, DEFAULT_API_URL } from "../nowsecure-client";
 import nock from "nock";
 import path from "path";
+import { HttpClientError } from "@actions/http-client";
 
 const platformToken = "AAABBB";
 const assessmentId = "CCCDDD";
@@ -22,7 +23,8 @@ describe("SARIF conversion", () => {
 
   test("can perform conversion", async () => {
     const scope = nock(DEFAULT_API_URL)
-      .post("/graphql")
+      .get("/graphql")
+      .query(true)
       .replyWithFile(
         200,
         path.join(__dirname, "resources", "response_200.json")
@@ -31,5 +33,40 @@ describe("SARIF conversion", () => {
     const report = await ns.pullReport(assessmentId);
     const sarif = await convertToSarif(report);
     expect(sarif).toMatchSnapshot();
+  });
+
+  test.each([1, 2, 3])(
+    "performs retries for 502 errors (%d times)",
+    async (times) => {
+      const scope1 = nock(DEFAULT_API_URL)
+        .get("/graphql")
+        .query(true)
+        .times(times)
+        .reply(502);
+
+      const scope2 = nock(DEFAULT_API_URL)
+        .get("/graphql")
+        .query(true)
+        .replyWithFile(
+          200,
+          path.join(__dirname, "resources", "response_200.json")
+        );
+
+      const report = await ns.pullReport(assessmentId);
+      const sarif = await convertToSarif(report);
+      expect(sarif).toMatchSnapshot();
+      expect(scope1.isDone());
+      expect(scope2.isDone());
+    }
+  );
+
+  test("request fails after 4 consequtive 502 errors", async () => {
+    const scope1 = nock(DEFAULT_API_URL)
+      .get("/graphql")
+      .query(true)
+      .times(4)
+      .reply(502);
+
+    expect(ns.pullReport(assessmentId)).rejects.toThrow(HttpClientError);
   });
 });
