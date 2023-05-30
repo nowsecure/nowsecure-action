6a880bbb966ef4ee92ab88b316b888fde0463a12
Keegan Saunders <ksaunders@nowsecure.com>
2023-05-30 15:38:00 -0400

    Use GitHub's dependency snapshot library

--- src/nowsecure-snapshot.ts
+++ src/nowsecure-snapshot.ts
@@ -4,31 +4,16 @@
  * SPDX-License-Identifier: MIT
  */
 
-import type { DependencySnapshot, Manifest } from "./types/dependency-snapshot";
+import {
+  Snapshot,
+  Manifest,
+  Package,
+} from "@github/dependency-submission-toolkit";
 import type { Deputy } from "./types/deputy";
+import type { Context } from "@actions/github/lib/context";
 import * as client from "@actions/http-client";
 import { version } from "./nowsecure-version";
 import { USER_AGENT } from "./nowsecure-client";
-
-/**
- * Contains environmental information for conversion routines.
- */
-export interface ActionContext {
-  sha: string;
-  ref: string;
-  job: string;
-  runId: number;
-  repo: {
-    owner: string;
-    repo: string;
-  };
-}
-
-/**
- * Upload URL for snapshots.
- */
-const snapshotUrl = (owner: string, repo: string) =>
-  `https://api.github.com/repos/${owner}/${repo}/dependency-graph/snapshots`;
 
 function encodePurl(ecosystem: string, name: string, version?: string): string {
   let purl = `pkg:${ecosystem}/${name}`;
@@ -45,9 +30,9 @@ function encodePurl(ecosystem: string, name: string, version?: string): string {
 export function convertToSnapshot(
   deputy: Deputy,
   githubCorrelator: string,
-  { ref, sha, job, runId }: ActionContext
-): DependencySnapshot {
-  const manifests = new Map<string, Manifest>();
+  context: Context
+): Snapshot {
+  const manifests: Record<string, Manifest> = {};
 
   for (const component of deputy.components) {
     let source = component.source;
@@ -55,12 +40,9 @@ export function convertToSnapshot(
       source = source.substr(1);
     }
 
-    let manifest = manifests.get(source);
+    let manifest = manifests[source];
     if (!manifest) {
-      manifest = {
-        name: source,
-        resolved: {},
-      };
+      manifest = new Manifest(source);
     }
 
     // We need to translate our ecosystem into a proper PURL type.
@@ -84,58 +66,18 @@ export function convertToSnapshot(
 
     const purl = encodePurl(ecosystem, component.name, component.version);
 
-    manifest.resolved[component.name] = {
-      package_url: purl.toString(),
-      relationship: "direct",
-    };
-
-    manifests.set(source, manifest);
+    manifest.addDirectDependency(new Package(purl));
+    manifests[source] = manifest;
   }
 
-  const snapshot: DependencySnapshot = {
-    version: 0,
-    ref,
-    sha,
-    job: {
-      correlator: githubCorrelator,
-      id: `${runId}`,
-    },
-    detector: {
+  const snapshot = new Snapshot(
+    {
+      version,
       name: "NowSecure",
       url: "https://www.nowsecure.com/",
-      version,
     },
-    scanned: new Date().toISOString(),
-    manifests: Object.fromEntries(manifests),
-  };
-
-  return snapshot;
-}
-
-/**
- * Submit a Snapshot to GitHub.
- *
- * NB: This code is meant to be temporary. Once GitHub releases an SBOM upload action a la
- * `upload-sarif`, this code will be removed and replaced with that.
- */
-export async function submitSnapshotData(
-  data: DependencySnapshot,
-  { repo: { owner, repo } }: ActionContext,
-  token: string
-) {
-  const httpClient = new client.HttpClient(USER_AGENT);
-  const r = await httpClient.post(
-    snapshotUrl(owner, repo),
-    JSON.stringify(data),
-    {
-      Accept: "application/vnd.github+json",
-      Authorization: `token ${token}`,
-    }
+    context
   );
-
-  if (r.message.statusCode !== 201) {
-    throw new Error(
-      `Snapshot request failed with status ${r.message.statusCode}`
-    );
-  }
+  snapshot.manifests = manifests;
+  return snapshot;
 }
